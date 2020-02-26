65cf6eaf20bd9fe08d6a9f06901d838cfc2f005f
palu3492 <palu3492@stthomas.edu>
2020-02-25 21:52:47 -0600

    Third problem

--- Problem2/myarr.c
+++ Problem2/myarr.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 int main (int argc, char **argv) {
-	// Int array size 20
-	int fibNums[20] = {0, 1}; // First two Fibonacci numbers
-	// Fill array with remaining 18 Fibonacci numbers
+	int fibNums[20] = {0, 1}; // Int array size 20 with first two Fibonacci numbers
+	// Fill array with remaining 18 numbers
 	for(int i = 2; i<20; i++){
 		fibNums[i] = fibNums[i-1] + fibNums[i-2];
 	}
---
+++ Problem3/myarr.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void copyArray(int *fibNums, int *my_arr2, int size);
+
+int main(int argc, char **argv) {
+	int fibNums[20] = {0, 1}; // Int array size 20 with first two Fibonacci numbers
+	// Fill array with remaining 18 numbers
+	for(int i = 2; i<20; i++){
+		fibNums[i] = fibNums[i-1] + fibNums[i-2];
+	}
+	printf("First 20 Fibonacci numbers:\n");
+	// Print first 19 numbers
+	for(int i = 0; i<19; i++){
+		printf("%d, ", fibNums[i]);
+	}
+	printf("%d\n", fibNums[19]); // Print last number
+	
+	// New for problem 3
+	int *my_arr2 = (int*)malloc(20*sizeof(int));
+	// Copy fibNums array into my_arr2 array
+	copyArray(fibNums, my_arr2, 20);
+	
+	
+	for(int i = 0; i<19; i++){
+		printf("%d, ", *(my_arr2+i));
+	}
+	printf("%d\n", *(my_arr2+19));
+	
+	
+	// Overwrite the contents of my_arr2 with the first 20 square numbers
+	for(int i = 0; i<20; i++){
+		*(my_arr2+i) = pow(i, 2);
+	}
+	// Print contents of my_arr2
+	printf("First 20 square numbers:\n");
+	for(int i = 0; i<19; i++){
+		printf("%d, ", *(my_arr2+i));
+	}
+	printf("%d\n", *(my_arr2+19));
+	
+	free(my_arr2); // free memory allocation
+	
+	return 0;
+}
+
+void copyArray(int *fibNums, int *my_arr2, int size){
+	memcpy(fibNums, my_arr2, size);
+}
